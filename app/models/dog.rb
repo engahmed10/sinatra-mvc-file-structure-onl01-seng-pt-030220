c77567232ca9e8780f8f5e5f95f23aa7111fad28
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-13 08:50:58 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,7 +3,7 @@ class Dog #< Sinatra::Base
   attr_accessor :name,:age,:breed
   @@all=[]
   def initialize(name,age,breed)
-    @name,@age,@breed = name,age,breed
+    @name,@breed,@age = name,age,breed
     @@all << self
   end
 
