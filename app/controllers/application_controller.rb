c4d922fa784ddf411f5dde572166fc70f4be406c
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-13 08:45:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,3 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 end
-
-
-class Dog #< Sinatra::Base
-  attr_accessor :name,:age,:breed
-  def initialize(name,age,breed)
-    @name,@age,@breed = name,age,breed
-  end
-
-end
